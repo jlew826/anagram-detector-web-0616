f75fd00f38cbb17e35fc7bc8841460d393fc0dab
jlew826 <jlew826@gmail.com>
2016-06-15 10:30:16 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,39 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word, :anagram
+
+  def initialize(word)
+    @word = word
+    @anagram = []
+  end
+
+  def match(phrase)
+
+    if phrase.include?(word)
+      anagram << phrase
+      
+    else
+      phrase.each_with_index do |element, index|
+        possible_anagram = phrase[index].split("")
+        if possible_anagram.sort == word.split("").sort
+          anagram << element
+        end
+        anagram
+      end
+    
+
+
+
+    end
+    anagram
+  end
+
+
+end
+
+
+
+
+
+
+
